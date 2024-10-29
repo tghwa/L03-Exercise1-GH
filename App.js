ee2bb87e4546ff3495d6c08a6dbdc3d08d6b019c
Gaik Hwa <23053627@myrp.edu.sg>
2024-10-29 16:32:56 +0800

    Work Done

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity, ToastAndroid } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    let name = '';
+    const [selectedUType, setUtype] = useState('');
+    return (
+        <View>
+            <Text></Text>
+            <Text style = {{color: 'grey'}}>User Type:</Text>
+            <RNPickerSelect
+                onValueChange={(value) => setUtype(value)}
+                items = {[
+                    {label: 'Admin', value: 'Admin'},
+                    {label: 'Guest', value: 'Guest'},
+                ]}
+            />
+
+            <Text style = {{color: 'grey'}}>User Name:</Text>
+                <TextInput style={{borderWidth: 1}} onChangeText={(text) => name=text} />
+
+            <Text style = {{color: 'grey'}}>Password:</Text>
+                <TextInput style={{borderWidth: 1}}></TextInput>
+
+            {/*Exercise 1c, d*/}
+            {/*<Button title = "LOG IN" onPress={ () => Alert.alert("Welcome!") } />*/}
+
+
+            <TouchableOpacity onPress={ () => ToastAndroid.show("Welcome " + selectedUType + ' ' + name, ToastAndroid.SHORT) }>
+                <Text style = {{color: 'grey'}}>LOG IN</Text>
+            </TouchableOpacity>
+
+              <StatusBar style="auto" />
+        </View>
+      );
+    }
 
 const styles = StyleSheet.create({
   container: {
